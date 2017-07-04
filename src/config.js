816efcfe876f3af1ea54ebc08246d1b97aa14101
Doug Hawkinson <doug5solas@gmail.com>
2017-07-04 13:16:28 -0700

    inching on, but needing help

--- src/config.js
+++ src/config.js
@@ -4,10 +4,8 @@
 //  This file is tagged in .gitignore for security purposes  
 
 module.exports = {
-    consumerKey         : "xdFe0zuOomtxYIhw3nMuatyMb",                            //  API Key
-    consumerSecret      : "MeIgvxmM5SwteYe4uMLL57eWedtkVlNg0JfhFyw3nzwLrvVy5k",   //  API Secret
-    accessToken         : "169299431-0UwnT2asUKT3fZPXiEFKV6ItD4gVOHkEdXzMMukd",   //  OAuth Access Token
-    accessTokenSecret   : "UUeue2PUKZD6f5TXSXRDT6gTrOqHEdyZSGSte80NE1RdE",        //  OAuth Access Token Secret
-    owner               :   "doug5solas",
-    ownerId             :   "169299431",
+    consumer_key         : "xdFe0zuOomtxYIhw3nMuatyMb",                            //  API Key
+    consumer_secret      : "MeIgvxmM5SwteYe4uMLL57eWedtkVlNg0JfhFyw3nzwLrvVy5k",   //  API Secret
+    access_token         : "169299431-0UwnT2asUKT3fZPXiEFKV6ItD4gVOHkEdXzMMukd",   //  OAuth Access Token
+    access_token_secret  : "UUeue2PUKZD6f5TXSXRDT6gTrOqHEdyZSGSte80NE1RdE",        //  OAuth Access Token Secret
 };
